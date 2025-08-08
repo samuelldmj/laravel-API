f89cfa8e39ae4efb7073fb4ff1d5db2653a2a609
samuelldmj <samuelldmj5@gmail.com>
2025-08-08 12:02:36 +0100

    models and migration files for blog api

---
+++ database/migrations/2025_08_08_094842_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('thumbnail')->nullable();
+            $table->text('excerpt')->nullable();
+            $table->longText('content');
+            $table->enum('status', ['draft', 'published', 'archived'])->default('draft');
+            $table->dateTime('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
